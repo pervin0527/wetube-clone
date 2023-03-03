1a153c8f48df64e00abfed6e49a38ae50adfc032
pervin0527 <zfbtldnz@gmail.com>
2023-03-03 19:01:53 +0900

    Updated 2023.03.03

--- src/server.js
+++ src/server.js
@@ -1,24 +1,20 @@
-// node_modules에서 express를 찾는다는 것을 npm이 이해하고 수행.
-// const express = require("express"); 와 동일.
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express(); // create express application.
+const logger = morgan("dev");
 
-// addEventListener의 callback 함수가 event 인수 값을 js로부터 받는 것과 동일.
-// request에 대한 object와 response에 대한 object를 express로부터 받는다.
 const handleHome = (req, res) => {
-    // 브라우저는 request를 보내고 서버로부터의 response를 받기 전까지 계속해서 대기 중.
-    // return res.end(); // request를 종료시켜버린다. kill request.
-    return res.send("This is Response.");
-
+    return res.send("<h1>This is Response.</h1>");
 };
-app.get("/", handleHome);
 
-// route 추가.
 const handleLogin = (req, res) => {
-    return res.send("This is Login page.");
+    return res.send({message : "This is Login page."});
 };
+
+app.use(logger);
+app.get("/", handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server Listening on Port http://localhost:${PORT}.`);
