c6877999400e8bd41c1e20e75fac4bba4719d532
pervin0527 <zfbtldnz@gmail.com>
2023-03-04 19:06:23 +0900

    Updated 2023.03.04

---
+++ README.md
@@ -0,0 +1,19 @@
+## Global Router
+
+/ -> Home  
+/join -> Join  
+/login -> Login  
+/search -> Search
+
+## User Router
+
+/user/edit -> edit user  
+/user/delete -> delete user
+
+## Video Router
+
+/videos/watch -> watch video  
+/videos/edit -> edit video  
+/videos/delete -> delete video  
+/videos/comment -> comment on a video
+/videos/comment/delete -> delete a comment
--- src/server.js
+++ src/server.js
@@ -1,21 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express(); // create express application.
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-    return res.send("<h1>This is Response.</h1>");
-};
-
-const handleLogin = (req, res) => {
-    return res.send({message : "This is Login page."});
-};
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`Server Listening on Port http://localhost:${PORT}.`);
 app.listen(PORT, handleListening);
