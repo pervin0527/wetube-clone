037abce60db375118dec9e3d990f40a0c4d4dbea
pervin0527 <zfbtldnz@gmail.com>
2023-03-08 20:40:13 +0900

    Updated 2023.03.08

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,32 @@
-const fakeUser = {
-    username : "Jun",
-    loggedIn : true,
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "Video #1",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1,
+        },
+        {
+            title: "Video #2",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1,
+        },
+        {
+            title: "Video #3",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1,
+        },
+    ];
+    res.render("home", {pageTitle : "Home", videos});
 }
-
-export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser});
 export const watchVideo = (req, res) => res.render("watch", {pageTitle : "Watch Video"});
 export const editVideo = (req, res) => res.render("edit", {pageTitle : "Edit Video"});
 export const deleteVideo = (req, res) => {
