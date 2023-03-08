4e0001c0e9d259606d50edcbfc50596beb0ed01d
pervin0527 <zfbtldnz@gmail.com>
2023-03-08 18:49:13 +0900

    Updated 2023.03.08(conditionals * variables)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const trending = (req, res) => res.render("home", {pageTitle : "Home"});
+const fakeUser = {
+    username : "Jun",
+    loggedIn : true,
+}
+
+export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser});
 export const watchVideo = (req, res) => res.render("watch", {pageTitle : "Watch Video"});
 export const editVideo = (req, res) => res.render("edit", {pageTitle : "Edit Video"});
 export const deleteVideo = (req, res) => {
