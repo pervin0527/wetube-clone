6adade629bbe349b5d145efb03bf89f61c4dd5cc
pervin0527 <zfbtldnz@gmail.com>
2023-03-09 23:51:45 +0900

    Updated 2023.03.09

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,47 @@
+let videos = [
+    {
+        title: "Video #1",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 1,
+        id : 1,
+    },
+    {
+        title: "Video #2",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 59,
+        id : 2,
+    },
+    {
+        title: "Video #3",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 59,
+        id : 3,
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "Video #1",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1,
-        },
-        {
-            title: "Video #2",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1,
-        },
-        {
-            title: "Video #3",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1,
-        },
-    ];
     res.render("home", {pageTitle : "Home", videos});
 }
-export const watchVideo = (req, res) => res.render("watch", {pageTitle : "Watch Video"});
-export const editVideo = (req, res) => res.render("edit", {pageTitle : "Edit Video"});
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    res.send("Remove Video");
+export const watchVideo = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    res.render("watch", {pageTitle : `Watching : ${video.title}`, video});
 }
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
+
+// form을 화면에 보여주는 controller
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    res.render("edit", {pageTitle : `Editing : ${video.title}`, video});
+}
+
+// 변경사항을 저장해주는 controller
+export const postEdit = (req, res) => {
+
+}
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { watchVideo, editVideo, upload, deleteVideo } from "../controllers/videoController";
+import {watchVideo, getEdit, postEdit} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watchVideo);
-videoRouter.get("/:id(\\d+)/edit", editVideo);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
